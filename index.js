4869218bf3b2a854f539060e0c255c7f1cdc85f8
oenzopinheiro <oenzopinheiro@gmail.com>
2023-06-26 17:37:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const human =  document.querySelector('.human')
+const dino = document.querySelector('.dino')
+
+const jump = () => {
+
+    human.classList.add('jump')
+
+    setTimeout(() => {
+
+        human.classList.remove('jump')
+    }, 500)
+}
+
+
+const loop = setInterval(() => {
+
+    
+
+
+   const dinoPosition = dino.offsetLeft
+   const humanPosition = +window.getComputedStyle(human).bottom.replace('px', '')
+
+   if(dinoPosition <= 120 && dinoPosition > 0 && humanPosition < 80 ) {
+
+    dino.style.animation = 'none'
+    dino.style.left = `${humanPosition}px`
+
+
+    human.style.animation = 'none'
+    human.style.bottom = `${dinoPosition}px`
+
+
+    clearInterval(loop) 
+
+   }
+
+},10)
+
+
+document.addEventListener('click' , jump)
+document.addEventListener('keydown' , jump)
